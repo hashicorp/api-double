aa744500e154f5507b59b78ff9d750a0a7e7e9a7
John Cowen <jcowen@hashicorp.com>
2021-06-30 10:54:39 +0100

    Adds string accepting seed function and more control of logging and seeding

--- vars/index.js
+++ vars/index.js
@@ -1,8 +1,34 @@
-module.exports = function(locationFactory, range, env) {
+module.exports = function(locationFactory, range, env, fake) {
   env = typeof env === 'undefined' ? process.env : env;
   return function(vars) {
+    if((vars.cookies['HC_API_DOUBLE_LOCALE'] || '').length > 0) {
+      fake.locale = vars.cookies['HC_API_DOUBLE_LOCALE'];
+    }
+    if (typeof seed === 'undefined') {
+      seed = fake.datatype.number();
+    } else {
+      seed = parseInt(seed);
+    }
+    if((vars.cookies['HC_API_DOUBLE_SEED'] || '').length > 0) {
+      seed = vars.cookies['HC_API_DOUBLE_SEED'];
+    }
+
+    // helper to get a deterministic number from a string if it isn't already a number
+    const seedStr = function(s) {
+      if(!isNaN(s)) {
+        return parseInt(s);
+      }
+      for(var i = 0, h = 0xdeadbeef; i < s.length; i++)
+          h = Math.imul(h ^ s.charCodeAt(i), 2654435761);
+      return (h ^ h >>> 16) >>> 0;
+    };
+    fake.seed(seedStr(seed));
     return Object.assign(vars, {
+      fake: fake,
       location: locationFactory(vars.href, vars.query),
+      seed: function(seed) {
+        fake.seed(seedStr(seed));
+      },
       range: function(a, b) {
         if (typeof b !== 'undefined') {
           b = parseInt(b);
